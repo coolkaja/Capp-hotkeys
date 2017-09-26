954e3700d486e8bdb39056c7276c35cdf84dd723
coolkaja <badgassie@gmail.com>
2017-09-26 11:06:05 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,74 @@
-function helloWorld (){
-	console.log('Hello world.');
-}
+//icon source: https://imgur.com/a/J6bTP
+var statusImage = document.createElement("img");
+statusImage.src = "https://i.imgur.com/CCjMY9U.png";
+statusImage.style.position = "fixed";
+statusImage.style.left = "4px";
+statusImage.style.bottom = "4px";
+document.getElementById("body").appendChild(statusImage);
+
+var runScript = true;
+document.onkeypress = function (evt) {
+  evt = evt || window.event;
+  var charCode = evt.keyCode || evt.which;
+  var charStr = String.fromCharCode(charCode);
+  //console.log(charCode);
+  if (charCode == 19) { //if pause|break is pressed
+    if (runScript){
+      runScript = false;
+      statusImage.src = "https://i.imgur.com/M9LM3d5.png";
+    }else{
+      runScript = true;
+      statusImage.src = "https://i.imgur.com/CCjMY9U.png";
+    }
+    console.log('Script status: ' + runScript);
+  }
+  if (runScript) {
+    switch (charCode) {
+      case 33: // ! opens "Evaluaties" tab
+        document.getElementById('dxtcDetailTabs_T11T').click();
+        break;
+      case 35: // # opens "Kenmerken" tab
+        document.getElementById('dxtcDetailTabs_T13T').click();
+        break;
+      case 36: // # opens "Materiaal" tab
+        document.getElementById('dxtcDetailTabs_T14T').click();
+        break;
+      case 49: // 1 opens "planning" tab
+        document.getElementById('dxtcDetailTabs_T0T').click();
+        break;
+      case 50: // 2 opens "Voorkennis" tab
+        document.getElementById('dxtcDetailTabs_T1T').click();
+        break;
+      case 51: // 3 opens "Activiteiten" tab
+        document.getElementById('dxtcDetailTabs_T2T').click();
+        break;
+      case 52: // 4 opens "Communicatie" tab
+        document.getElementById('dxtcDetailTabs_T3T').click();
+        break;
+      case 53: // 5 opens "Financieel" tab
+        document.getElementById('dxtcDetailTabs_T4T').click();
+        break;
+      case 54: // 6 opens "Doelgroepen" tab
+        document.getElementById('dxtcDetailTabs_T5T').click();
+        break;
+      case 55: // 7 opens "Vakgebieden" tab
+        document.getElementById('dxtcDetailTabs_T6T').click();
+        break;
+      case 56: // 8 opens "Gedifferentieerd aanbod" tab
+        document.getElementById('dxtcDetailTabs_T7T').click();
+        break;
+      case 57: // 9 opens "Punten" tab
+        document.getElementById('dxtcDetailTabs_T9T').click();
+        break;
+      case 48: // 0 opens "Beoordelingen" tab
+        document.getElementById('dxtcDetailTabs_T10T').click();
+        break;
+      case 64: // @ opens "Evaluaties" tab
+        document.getElementById('dxtcDetailTabs_T12T').click();
+        break;
+      default:
+        //console.log(charCode + '/' + charStr + 'is unbound.');
+        break;
+    }
+  }
+};
