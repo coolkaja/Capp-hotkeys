4e0662c916f1493ccf7ea8b1918b082e4581718a
coolkaja <badgassie@gmail.com>
2017-12-08 11:21:56 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,38 +1,83 @@
-statusImage = document.getElementById("statusImage");
-statusImage.src = "https://i.imgur.com/CCjMY9U.png";
+//a bunch of functions used by the scripts
 
-var runScript = true;
-document.onkeypress = function (evt) {
-  evt = evt || window.event;
-  var charCode = evt.keyCode || evt.which;
-  var charStr = String.fromCharCode(charCode);
-  //console.log(charCode);
-  if (charCode == 19) { //if pause|break is pressed
-    if (runScript){
-      runScript = false;
-      statusImage.src = "https://i.imgur.com/M9LM3d5.png";
-    }else{
-      runScript = true;
-      statusImage.src = "https://i.imgur.com/CCjMY9U.png";
-    }
-    console.log('Script status: ' + runScript);
+//checks if function is called inside an iframe
+//returns true if it's in an iframe, and false if it isn't
+function inIframe () {
+  try {
+    return window.self !== window.top;
+  } catch (e) {
+    return true;
   }
-  if (runScript) {
-    switch (charCode) {
-      case 49: // 1 activates the "bewerken" button.
-        location.assign(document.getElementsByClassName("Button Button--default Button--small Button--group").item(0).href);
-        break;
-      case 50: // 2 activates the personen koppelen button
-        location.assign(document.getElementsByClassName("Button Button--default Button--small Button--group").item(1).href);
-        break;
-      case 51: // 3 activates the second "bewerken" button.
-        location.assign(document.getElementsByClassName("Button Button--default Button--small Button--group").item(2).href);
-        break;
-      case 116: // t goes back three pages
-        window.history.go(-3);
-      default:
-        console.log(charCode + '/' + charStr + ' is unbound.');
-        break;
-    }
+}
+
+//adds a status image to the current page
+//returns the object if it succefully created it, returns false if it didn't
+function addStatusIcon(){
+	if (!inIframe()){
+		var statusImageLink = document.createElement("a")
+		statusImageLink.href = "https://github.com/coolkaja/Capp-hotkeys/blob/master/README.md#" + getPageName().toLowerCase();
+		statusImageLink.target = "_blank";
+		document.getElementsByTagName("body").item(0).appendChild(statusImageLink);
+
+		var statusImage = document.createElement("img");
+		statusImage.src = "https://i.imgur.com/SimKpiX.png";
+		statusImage.style.position = "fixed";
+		statusImage.style.left = "4px";
+		statusImage.style.bottom = "4px";
+		statusImage.setAttribute("id", "statusImage");
+		statusImageLink.appendChild(statusImage);
+		return statusImageLink;
+	}
+	else {
+		return false;
+	}
+}
+
+//get the name of the page
+//returns the name of the current page according to the naming scheme of capp
+function getPageName(){
+  adress = window.location.href;
+  adress = adress.split(".");
+  if (adress[0] == "https://staging"){
+    adress = adress[2];
+    adressarr = adress.split("/");
+    adressarr.splice(0, 0);
   }
-};
+  else{
+    adress = adress[2];
+    adress.split("/");
+    adressarr = adress;
+  }
+  
+  
+  
+  n = "";
+  for(i = 2; i < adressarr.length; i++){
+	  if (/^[a-zA-Z]+$/.test(adressarr[i])){
+		  n += adressarr[i];
+	  }
+  }
+  return n;
+}
+
+//loads script from git
+//returns the script element it creates
+function addGitScript(name){
+  var imported = document.createElement('script');
+  imported.src = 'https://rawgit.com/coolkaja/Capp-hotkeys/master/' + name + '.js';
+  document.head.appendChild(imported);
+  return imported;
+}
+
+//add a hotkey script from git
+//returns true if it added it succefully and false if it's called in an iframe
+function addHotkeys(){
+  //exit if the script runs in an iframe.
+  if (inIframe()){
+    return false;
+  }
+  //get page name and run the script for that page.
+  addGitScript(getPageName());
+  return true;
+}
+
