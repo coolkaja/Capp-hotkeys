79a31b292924eb1a79bda37ff04d9d6ba3d8b775
coolkaja <badgassie@gmail.com>
2017-12-08 11:25:01 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//add my functions too the page
 var imported = document.createElement('script');
 imported.src = 'https://rawgit.com/coolkaja/Capp-hotkeys/master/functions.js';
 document.head.appendChild(imported);
