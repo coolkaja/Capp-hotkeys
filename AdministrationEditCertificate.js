c973aa96fa664aeeeb7cb9c161175f6d7f9aee43
coolkaja <badgassie@gmail.com>
2017-12-12 13:26:18 +0100

    Update AdministrationEditCertificate.js

--- AdministrationEditCertificate.js
+++ AdministrationEditCertificate.js
@@ -1,1 +1,33 @@
+statusImage = document.getElementById("statusImage");
+statusImage.src = "https://i.imgur.com/CCjMY9U.png";
 
+var runScript = true;
+document.onkeypress = function (evt) {
+  evt = evt || window.event;
+  var charCode = evt.keyCode || evt.which;
+  var charStr = String.fromCharCode(charCode);
+  //console.log(charCode);
+  if (charCode == 19) { //if pause|break is pressed
+    if (runScript){
+      runScript = false;
+      statusImage.src = "https://i.imgur.com/M9LM3d5.png";
+    }else{
+      runScript = true;
+      statusImage.src = "https://i.imgur.com/CCjMY9U.png";
+    }
+    console.log('Script status: ' + runScript);
+  }
+  if (runScript) {
+    switch (charCode) {
+      case 49: // 1 activates the "toevoegen" button
+        document.getElementById('btnAddBottom').click();
+        break;
+      case 50: // 2 activates the "bladeren" button
+        document.getElementById('grdEntities_dxdt1_grdCertificateClauseSpecs_1_col17').click();
+        break;
+      default:
+        console.log(charCode + '/' + charStr + ' is unbound.');
+        break;
+    }
+  }
+};
